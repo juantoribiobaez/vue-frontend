8643463edbc0260a5be45d2391962aa567a637be
Juan O Toribio Baez <juantoribiobaez@gmail.com>
2023-10-18 13:52:50 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../components/HomePage.vue'
 import LoginPage from '../views/LoginPage.vue'
+import RegisterPage from '../views/RegisterPage.vue'
 
 const routes = [
   {
@@ -14,6 +15,11 @@ const routes = [
     path: '/login',
     name: 'LoginPage',
     component: LoginPage
+  },
+  {
+    path: '/register',
+    name: 'RegisterPage',
+    component: RegisterPage
   }
 ]
 
